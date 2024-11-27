b3531181ce45e6a274050bb561c85cc54e51503b
Brahm Van Houzen <brahmvanh@gmail.com>
2024-11-26 20:16:33 -0500

    turned into library crate, created thread pool and worker implimentations

---
+++ src/bin/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+use std::io::prelude::*;
+use std::{ io::{ prelude::*, BufReader }, net::{ TcpListener, TcpStream } };
+use web_server_demo_rust_lang_book::ThreadPool;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established.");
+
+        handle_connection(stream);
+    }
+}
+
+// fn handle_connection(mut stream: TcpStream) {
+//     let buf_reader = BufReader::new(&mut stream);
+//     let http_request: Vec<_> = buf_reader
+//         .lines()
+//         .map(|result| result.unwrap())
+//         .take_while(|line| !line.is_empty())
+//         .collect();
+
+//     println!("Request: {http_request:#?}");
+// }
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    // println!("Request : {}", String::from_utf8_lossy(&buffer[..]));
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
